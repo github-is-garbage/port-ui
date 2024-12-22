df77b3cc43ce7b45f3c2c8ae2af63097c961887a
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-12-22 06:21:55 -0500

    Add vertical support to Sliders

--- lua/port-ui/elements/list/slider.lua
+++ lua/port-ui/elements/list/slider.lua
@@ -6,6 +6,7 @@ AccessorFunc(ELEMENT, "m_flValue", "Value", FORCE_NUMBER)
 AccessorFunc(ELEMENT, "m_iDecimalPoints", "DecimalPoints", FORCE_NUMBER)
 AccessorFunc(ELEMENT, "m_flMinimumValue", "MinimumValue", FORCE_NUMBER)
 AccessorFunc(ELEMENT, "m_flMaximumValue", "MaximumValue", FORCE_NUMBER)
+AccessorFunc(ELEMENT, "m_bVertical", "Vertical", FORCE_BOOL)
 
 function ELEMENT:Init()
 	self.m_DragData = {}
@@ -15,6 +16,7 @@ function ELEMENT:Init()
 	self:SetMaximumValue(1)
 
 	self:SetValue(0)
+	self:SetVertical(false)
 
 	-- Knob
 	local Base = FindMetaTable("portui_Base")
@@ -23,7 +25,6 @@ function ELEMENT:Init()
 	local Knob = portui.Elements.Create("Button")
 	Knob:SetParent(self)
 	Knob:SetText("")
-	Knob:SetWidth(10)
 	Knob:SetHasInputEnabled(false)
 
 	function Knob:PaintBackground(RenderWidth, RenderHeight, Width, Height)
@@ -61,36 +62,58 @@ function ELEMENT:Think()
 		return
 	end
 
-	local ClickX = (gui.MouseX() - self:GetRelativeX()) + self.m_DragData.MouseX
-	ClickX = self.m_DragData.MouseX - ClickX
-	ClickX = -ClickX -- It's backwards for some reason
+	local Vertical = self:GetVertical()
 
-	local ValueX = math.Remap(ClickX, 0, self:GetWidth(), self.m_flMinimumValue, self.m_flMaximumValue)
-	self:SetValue(ValueX)
+	local DragPos = Vertical and self.m_DragData.MouseY or self.m_DragData.MouseX
+	local MousePos = Vertical and gui.MouseY() or gui.MouseX()
+	local RelativePos = Vertical and self:GetRelativeY() or self:GetRelativeX()
+
+	local ClickPos = (MousePos - RelativePos) + DragPos
+	ClickPos = DragPos - ClickPos
+	ClickPos = -ClickPos -- It's backwards for some reason
+
+	local ValuePos = math.Remap(ClickPos, 0, Vertical and self:GetHeight() or self:GetWidth(), self.m_flMinimumValue, self.m_flMaximumValue)
+	self:SetValue(ValuePos)
 end
 
 function ELEMENT:PaintBackground() end
 
 function ELEMENT:PaintForeground(RenderWidth, RenderHeight)
 	surface.SetDrawColor(0, 0, 0, 255)
-	surface.DrawLine(0, RenderHeight * 0.5, RenderWidth, RenderHeight * 0.5)
+
+	if self:GetVertical() then
+		surface.DrawLine(RenderWidth * 0.5, 0, RenderWidth * 0.5, RenderHeight)
+	else
+		surface.DrawLine(0, RenderHeight * 0.5, RenderWidth, RenderHeight * 0.5)
+	end
 end
 
-function ELEMENT:OnLeftClick(MouseX)
+function ELEMENT:OnLeftClick(MouseX, MouseY)
 	self.m_DragData.MouseX = MouseX
-	-- self.m_DragData.MouseY = MouseY -- TODO: Vertical sliders
+	self.m_DragData.MouseY = MouseY
 
 	portui.Elements.Input.StartGrabbingInput(self)
 end
 
 function ELEMENT:PerformLayout(Width, Height)
+	local Vertical = self:GetVertical()
+
 	local ValuePercentage = math.Remap(self.m_flValue, self.m_flMinimumValue, self.m_flMaximumValue, 0, 1)
-	local ValueX = Width * ValuePercentage
 
-	ValueX = math.Clamp(ValueX, 0, Width - self.m_Knob:GetWidth())
+	local KnobWidth = Vertical and Width or (Height * 0.5)
+	local KnobHeight = Vertical and (Width * 0.5) or Height
+	local KnobBound = Vertical and Height or Width
 
-	self.m_Knob:SetX(ValueX)
-	self.m_Knob:SetHeight(Height)
+	local ValuePos = (Vertical and Height or Width) * ValuePercentage
+	ValuePos = math.Clamp(ValuePos, 0, KnobBound - (Vertical and KnobHeight or KnobWidth))
+
+	self.m_Knob:SetSize(KnobWidth, KnobHeight)
+
+	if Vertical then
+		self.m_Knob:SetY(ValuePos)
+	else
+		self.m_Knob:SetX(ValuePos)
+	end
 end
 
 portui.Elements.Register("Slider", ELEMENT, "Base")
