23c715b509c90af5b7a7183728ce62e1a34a7f3a
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-12-22 08:27:25 -0500

    Make the Colorbox show a PopupWindow changer

--- lua/port-ui/elements/list/color_picker/colorbox.lua
+++ lua/port-ui/elements/list/color_picker/colorbox.lua
@@ -11,4 +11,34 @@ function ELEMENT:PaintBackground(RenderWidth, RenderHeight)
 	surface.DrawRect(0, 0, RenderWidth, RenderHeight)
 end
 
+function ELEMENT:OnValueChanged(NewColor)
+	-- For override
+end
+
+function ELEMENT:OnLeftClick()
+	if IsValid(self.m_PopupWindow) then
+		self.m_PopupWindow:Remove()
+	end
+
+	local PopupWindow = portui.Elements.Create("PopupWindow")
+	PopupWindow:SetTitle("Choose Color")
+	PopupWindow:SetSize(200, 172)
+	self.m_PopupWindow = PopupWindow
+
+	local ColorPicker = PopupWindow:AddChild("ColorPicker", FILL)
+	ColorPicker:SetColor(self:GetColor())
+	ColorPicker.m_ColorBox = self
+
+	function ColorPicker:OnValueChanged(NewColor)
+		if not IsValid(self.m_ColorBox) then return end
+
+		self.m_ColorBox.m_Color.r = NewColor.r
+		self.m_ColorBox.m_Color.g = NewColor.g
+		self.m_ColorBox.m_Color.b = NewColor.b
+		self.m_ColorBox.m_Color.a = NewColor.a
+
+		self.m_ColorBox:OnValueChanged(self.m_ColorBox:GetColor())
+	end
+end
+
 portui.Elements.Register("Colorbox", ELEMENT, "Base")
